d506c93160b7a8aab364567384390f3802750af1
Rodrigo de Oliveira <rodrigo.oliveira@polimetri.com.br>
2020-10-24 12:48:14 -0300

    Commit inicial.

---
+++ buscador_gifs/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:buscador_gifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      hintColor: Colors.white,
+    ),
+    home: HomePage(),
+  ));
+}
